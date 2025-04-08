ae5f8dc9b9cb7a543125c18362872c3cd2cc4d2b
Anna Brumark <annahelenabrumark@gmail.com>
2025-04-08 18:40:55 +0200

    added remote pattern for images in next config

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
 import { getProjects } from "../../sanity/queries.js";
+import Image from "next/image";
 
 export default async function Home() {
   const projects = await getProjects();
@@ -8,7 +9,12 @@ export default async function Home() {
       {projects.map((project) => (
         <div key={project._id}>
           <p>{project.title}</p>
-         
+          <Image
+            src={project.image}
+            alt={project.title}
+            width={500}
+            height={500}
+          />
         </div>
       ))}
     </>
