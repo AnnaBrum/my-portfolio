d16a1f36563b0c2121d1ff1dceeb0ef2358023df
Anna Brumark <annahelenabrumark@gmail.com>
2025-04-10 15:07:58 +0200

    Added url to project schema

--- sanity/schemaTypes/projectType.js
+++ sanity/schemaTypes/projectType.js
@@ -11,7 +11,7 @@ export const projectType = defineType({
       type: "string",
     }),
     defineField({
-      name: "slug",
+      name: "slug", // a slug is the last part of a url
       description:
         "This field is the project page name at my-portfolio.com/projects/<name>.",
       title: "Slug",
@@ -23,13 +23,19 @@ export const projectType = defineType({
     defineField({
       name: "description",
       title: "Description",
-      type: "text",
+      type: "array",
+      of: [{ type: "block" }],
     }),
     defineField({
       name: "image",
       title: "Project Image",
       type: "image",
-      options: { hotspot: true },
+      options: { hotspot: true }, //to be able to have a image preview in the studio
+    }),
+    defineField({
+      name: "link",
+      title: "Project Link",
+      type: "url",
     }),
   ],
 });
